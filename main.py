b725bdae96938594758324749b58cbd7d7f19c2f
Najee Burnette <najeeburnette@gmail.com>
2022-12-19 18:38:28 -0800

    Added flask package and homepage route

--- main.py
+++ main.py
@@ -1,41 +1,53 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+from flask import Flask
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 from sklearn import metrics
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'This is the homepage'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 df = pd.read_csv("Housing.csv")
 
 # List of variables to map
-varlist =  ['mainroad', 'guestroom', 'basement',
-            'hotwaterheating', 'airconditioning', 'prefarea']
+varlist = ['mainroad', 'guestroom', 'basement',
+           'hotwaterheating', 'airconditioning', 'prefarea']
+
 
 # Binarizing yes/no category columns
 def binary_map(x):
     return x.map({'yes': 1, "no": 0})
 
-df[varlist] = df[varlist].apply(binary_map)
 
+df[varlist] = df[varlist].apply(binary_map)
 
 # Furnishing columm using label encoding
 le = LabelEncoder()
 df.furnishingstatus = le.fit_transform(df.furnishingstatus)
 
-
 # Implementaiton of Regression Model RFR
-x= df.iloc [:,1:]
-y= df.iloc [:,:1]
+x = df.iloc[:, 1:]
+y = df.iloc[:, :1]
 
+regressor = RandomForestRegressor(n_estimators=100, random_state=0)
 
-regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
-
-regressor.fit(x.values,y.values.ravel())
+regressor.fit(x.values, y.values.ravel())
 
 # test the output by changing values
-y_pred = regressor.predict(np.array([3500, 3, 2, 1, 1, 0, 0, 1, 1, 2, 0, 0]).reshape(1, 12)) 
+y_pred = regressor.predict(np.array([3500, 3, 2, 1, 1, 0, 0, 1, 1, 2, 0, 0]).reshape(1, 12))
 
+'''
 estimate_int = y_pred.astype(np.int64)
 estimate_str = str(estimate_int[0])
 print("Your estimated price is : " + estimate_str)
 print(regressor.score(x.values,y.values))
+'''
